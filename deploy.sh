df26c02af60bb0a49888e83c4ebe6561e1f6f743
Arian Ebrahimi <ae@retal.tech>
2023-04-21 06:08:26 +0330

    F

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,13 @@
-ng build --base_href /neghabkaraj.ir/
+git branch -D dist&&
+git branch dist&&
+git checkout dist&&
+git merge master&&
+npm i && npm audit fix &&
+ng build&&
+rm *.json node_modules src front *.MD *.md -rf
+mv dist/kernelious/* . &&
+rm dist -rf
+rm README.md
+git add -A
+git commit -m "Deploy"
+git push -f  origin dist
