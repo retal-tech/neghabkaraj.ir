b9dc1c3cf7ba448a574619765c6e3bfefcbf22f6
Arian Ebrahimi <ae@retal.tech>
2023-04-21 05:38:29 +0330

    Update

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component'
+
+const routes: Routes = [
+  {path:"home", component: HomeComponent},
+  {path: "", component: HomeComponent},
+  {path: "**", component: HomeComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, {scrollPositionRestoration: 'enabled'})],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
